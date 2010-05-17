43fc20bee2f5a7723f3c039267b83c401b6d04b8
Hervé Jégou <rvj@fb.com>
2010-05-17 21:54:52 +0000

    fixed test program parameter definition

--- test/test_yael_kmeans.m
+++ test/test_yael_kmeans.m
@@ -5,6 +5,7 @@ d = 128;                   % dimensionality of the vectors
 n = 100000;                % number of vectors
 nt = 2;                    % number of threads to use
 v = single (randn (d, n)); % random set of vectors 
+niter = 30;
 
-[centroids, dis, assign] = yael_kmeans (v, 100, 'nt', 2, 'niter', 25);
+[centroids, dis, assign] = yael_kmeans (v, k, 'nt', nt, 'niter', niter);
 
