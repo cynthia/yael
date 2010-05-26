33897a06ae508f7adbe78b75e84271c3eda613da
Hervé Jégou <rvj@fb.com>
2010-05-26 14:46:58 +0000

    graphics change update fvecs (able to choose a subrange of vectors)

--- matlab/fvecs_read.m
+++ matlab/fvecs_read.m
@@ -4,7 +4,8 @@
 % Syntax: 
 %   v = fvecs_read (filename)     -> read all vectors
 %   v = fvecs_read (filename, n)  -> read n vectors 
-function v = fvecs_read (filename, maxn)
+%   v = fvecs_read (filename, [a b]) -> read the vectors from a to b (indices starts from 1)
+function v = fvecs_read (filename, bounds)
 
 % open the file and count the number of descriptors
 fid = fopen (filename, 'rb');
@@ -15,19 +16,43 @@ end
 
 % Read the vector size
 d = fread (fid, 1, 'int');
+vecsizeof = 1 * 4 + d * 4;
 
-if nargin < 2
-  fseek (fid, 0, 1);
-  n = ftell (fid) / (1 * 4 + d * 4);
-else
-  n = maxn;
+% Get the number of vectrors
+fseek (fid, 0, 1);
+a = 1;
+bmax = ftell (fid) / vecsizeof;
+b = bmax;
+
+if nargin >= 2
+  if length (bounds) == 1
+    b = bounds;
+   
+  elseif length (bounds) == 2
+    a = bounds(1);
+    b = bounds(2);    
+  end
 end
-  
-fseek (fid, 0, -1);
 
-% first read the meta information associated with the descriptor
+assert (a >= 1);
+if b > bmax
+  b = bmax;
+end
+
+[a b bmax]
+if b == 0 | b < a
+  v = [];
+  fclose (fid);
+  return;
+end
+
+% compute the number of vectors that are really read and go in starting positions
+n = b - a + 1;
+fseek (fid, (a - 1) * vecsizeof, -1);
+
+% read n vectors
 v = fread (fid, (d + 1) * n, 'float=>single');
 v = reshape (v, d + 1, n);
 v = v (2:end, :);
-
+  
 fclose (fid);
