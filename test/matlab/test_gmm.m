59161428640a36e83f2a4115a7801fb5a458bc7e
Hervé Jégou <rvj@fb.com>
2010-06-29 00:35:52 +0000

    minor modif

--- test/matlab/test_gmm.m
+++ test/matlab/test_gmm.m
@@ -5,14 +5,14 @@ d = 128;                   % dimensionality of the vectors
 n = 10000;                 % number of vectors
 v = single(rand (d, n));   % random set of vectors 
 niter = 30;                % maximum number of iterations
-verbose = 1;               % verbosity level
+verbose = 2;               % verbosity level
 seed = 3;                  % 0: no seeding, values > 0 are used as seed
-nt = 2;                    % to force multi-threading if not done by Matlab/octave
+nt = 1;                    % to force multi-threading if not done by Matlab/octave
                            % check if multithreaded actived with nt=1 before 
 			   % changing this variable
 			   
 
 tic
-[w, mu, sigma] = yael_gmm (v, k, 'niter', niter, 'nt', nt, ...
+[w, mu, sigma] = yael_gmm (v, k, 'niter', niter, ...
 			   'verbose', verbose, 'seed', seed);
 toc
