269f848248298f0821073790b24035a0549f2679
Hervé Jégou <rvj@fb.com>
2010-09-21 14:20:11 +0000

    improved version of ivecs_read (with bounds)

--- matlab/ivecs_read.m
+++ matlab/ivecs_read.m
@@ -1,9 +1,11 @@
 % Read a set of vectors stored in the ivec format (int + n * int)
 % The function returns a set of output vector (one vector per column)
 %
-% Usage: v = ivecs_read (filename)    -> read all vectors
-% Usage: v = ivecs_read (filename, n) -> read n vectors
-function v = ivecs_read (filename, maxn)
+% Syntax: 
+%   v = ivecs_read (filename)     -> read all vectors
+%   v = ivecs_read (filename, n)  -> read n vectors 
+%   v = ivecs_read (filename, [a b]) -> read the vectors from a to b (indices starts from 1)
+function v = ivecs_read (filename, bounds)
 
 % open the file and count the number of descriptors
 fid = fopen (filename, 'rb');
@@ -14,20 +16,46 @@ end
 
 % Read the vector size
 d = fread (fid, 1, 'int');
+vecsizeof = 1 * 4 + d * 4;
 
-if nargin < 2
-    fseek (fid, 0, 1);
-    n = ftell (fid) / (1 * 4 + d * 4);
-else
-  n = maxn;
+% Get the number of vectrors
+fseek (fid, 0, 1);
+a = 1;
+bmax = ftell (fid) / vecsizeof;
+b = bmax;
+
+if nargin >= 2
+  if length (bounds) == 1
+    b = bounds;
+   
+  elseif length (bounds) == 2
+    a = bounds(1);
+    b = bounds(2);    
+  end
 end
-fseek (fid, 0, -1);
- 
-% first read the meta information associated with the descriptor
-v = zeros (n, d);
 
-v = fread (fid, (d + 1) * n, 'int=>double');
+assert (a >= 1);
+if b > bmax
+  b = bmax;
+end
+
+if b == 0 | b < a
+  v = [];
+  fclose (fid);
+  return;
+end
+
+% compute the number of vectors that are really read and go in starting positions
+n = b - a + 1;
+fseek (fid, (a - 1) * vecsizeof, -1);
+
+% read n vectors
+v = fread (fid, (d + 1) * n, 'float=>double');
 v = reshape (v, d + 1, n);
-v = v (2:end, :);
 
+% Check if the first column (dimension of the vectors) is correct
+assert (sum (v (1, 2:end) == v(1, 1)) == n - 1);
+v = v (2:end, :);
+  
 fclose (fid);
+
