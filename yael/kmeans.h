2542bfea9fee5d6b7add6bfe37caeb1c5c2be3ad
Hervé Jégou <rvj@fb.com>
2011-11-07 23:52:40 +0000

    yo

--- yael/kmeans.h
+++ yael/kmeans.h
@@ -8,10 +8,10 @@
 
 
 /* flags for kmeans */
-#define KMEANS_INIT_RANDOM              0x00000
 #define KMEANS_QUIET                    0x10000
 #define KMEANS_INIT_BERKELEY            0x20000
 #define KMEANS_NORMALIZE_CENTS          0x40000
+#define KMEANS_INIT_RANDOM              0x80000
 #define KMEANS_INIT_USER               0x100000
 
 #define KMEANS_L1                       0x200000
