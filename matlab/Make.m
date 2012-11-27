acd072ff58310c669022dea95f5a7b38ce4407e7
Hervé Jégou <rvj@fb.com>
2012-11-27 20:42:15 +0000

    Makefile for some important Yael Matlab functions

---
+++ matlab/Make.m
@@ -0,0 +1,14 @@
+
+
+setenv ('YAELCFLAGS', '-msse4 -fopenmp -I/usr/include -I.. -DFINTEGER=long -DHAVE_ARPACK');
+setenv ('YAELLDFLAGS', '-lmwblas -lmwlapack -lmwarpack -fopenmp');
+
+mex -g CFLAGS="\$CFLAGS \$YAELCFLAGS" LDFLAGS="\$LDFLAGS \$YAELLDFLAGS" yael_kmeans.c ../yael/kmeans.c ../yael/vector.c ../yael/machinedeps.c ../yael/binheap.c ../yael/nn.c ../yael/sorting.c
+
+mex -g CFLAGS="\$CFLAGS \$YAELCFLAGS" LDFLAGS="\$LDFLAGS \$YAELLDFLAGS" yael_nn.c ../yael/vector.c ../yael/machinedeps.c ../yael/binheap.c ../yael/nn.c ../yael/sorting.c
+
+mex -g CFLAGS="\$CFLAGS \$YAELCFLAGS" LDFLAGS="\$LDFLAGS \$YAELLDFLAGS" yael_kmax.c ../yael/vector.c ../yael/machinedeps.c ../yael/binheap.c ../yael/sorting.c
+
+mex -g CFLAGS="\$CFLAGS \$YAELCFLAGS" LDFLAGS="\$LDFLAGS \$YAELLDFLAGS" yael_kmin.c ../yael/vector.c ../yael/machinedeps.c ../yael/binheap.c ../yael/sorting.c
+
+!rm *.o
