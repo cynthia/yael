a6d70a3d74b26dfe43f08ea672a55b1ab3f022ab
Hervé Jégou <rvj@fb.com>
2013-09-24 13:16:19 +0000

    multi-thread yael_fvecs_normalize

--- matlab/yael_fvecs_normalize.c
+++ matlab/yael_fvecs_normalize.c
@@ -5,10 +5,10 @@ void mexFunction (int nlhs, mxArray *plhs[],
                   int nrhs, const mxArray*prhs[])
 
 {
-  if (nrhs < 1 || nrhs > 4) 
+  if (nrhs < 1 && nrhs > 4) 
     mexErrMsgTxt ("Invalid number of input arguments");
   
-  if (nlhs < 1 || nlhs > 2)
+  if (nlhs < 1 && nlhs > 2)
     mexErrMsgTxt ("1 o 2 output arguments are required");
 
   if (mxGetClassID(prhs[0]) != mxSINGLE_CLASS)
@@ -34,6 +34,8 @@ void mexFunction (int nlhs, mxArray *plhs[],
   double * vnr = (double*) mxGetPr (plhs[1]);
 
 
+#ifdef _OPENMP
+#pragma omp parallel for private (i)
   for (i = 0 ; i < n ; i++) {
     double nrtmp = fvec_norm (v + i * d, d, norm);
     vnr[i] = nrtmp;
@@ -43,4 +45,15 @@ void mexFunction (int nlhs, mxArray *plhs[],
     for (j = 0 ; j < d ; j++)
       vo[i*d+j] = v[i*d+j] * nrtmp;
   }
+#else
+  for (i = 0 ; i < n ; i++) {
+    double nrtmp = fvec_norm (v + i * d, d, norm);
+    vnr[i] = nrtmp;
+    nrtmp = 1 / nrtmp;
+    
+    long j = 0;
+    for (j = 0 ; j < d ; j++)
+      vo[i*d+j] = v[i*d+j] * nrtmp;
+  }  
+#endif
 }
