78d8926fe0a92c5e2fc58986e528bd3a0c176f53
Hervé Jégou <rvj@fb.com>
2014-07-04 12:00:12 +0000

    minor

--- matlab/Configure.m
+++ matlab/Configure.m
@@ -1,5 +1,5 @@
 % Compilation parameters
-openmp = true;   % De-activated by default since latest MacOS needs hacking to have it working
+openmp = false;   % De-activated by default since latest MacOS needs hacking to have it working
 sse4 = true;
 arpack = true;
 
